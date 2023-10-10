93f61e14de241de19dfa461d0ac91e13005fd523
Juan Ignacio Meza <electronica2057@gmail.com>
2023-10-10 15:13:57 -0300

    Implementación balanceo de carga: Random

--- checkout-service/src/main/java/com/example/demo/repository/FeignProductRepository.java
+++ checkout-service/src/main/java/com/example/demo/repository/FeignProductRepository.java
@@ -1,12 +1,14 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Product;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "products-service")
+@LoadBalancerClient(value = "product-service",configuration = LoadBalancerClient.class)
 public interface FeignProductRepository {
 
     @RequestMapping(method = RequestMethod.GET, value = "/products")
